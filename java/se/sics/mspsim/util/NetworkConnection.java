251ad6157bf930aee55d1bcaaa27816ec4f13575
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-08-25 10:42:00 +0200

    Remove unused imports

--- java/se/sics/mspsim/util/NetworkConnection.java
+++ java/se/sics/mspsim/util/NetworkConnection.java
@@ -41,7 +41,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import se.sics.mspsim.chip.PacketListener;
