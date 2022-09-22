cb79ee08ad1481195446e8a2b4f1254073ec0d5a
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-09-22 15:03:36 +0200

    Cooja: invert one more if-statement
    This turns the program flow into a sequence
    of statements where the code returns if something
    fails or the user cancels.

--- java/org/contikios/cooja/Cooja.java
+++ java/org/contikios/cooja/Cooja.java
@@ -2570,37 +2570,34 @@ public class Cooja extends Observable {
     mySimulation.stopSimulation();
 
     JFileChooser fc = newFileChooser();
+    if (fc.showSaveDialog(myDesktopPane) != JFileChooser.APPROVE_OPTION) {
+      return null;
+    }
 
-    int returnVal = fc.showSaveDialog(myDesktopPane);
-    if (returnVal == JFileChooser.APPROVE_OPTION) {
-      File saveFile = fc.getSelectedFile();
-      if (!fc.accept(saveFile)) {
-        saveFile = new File(saveFile.getParent(), saveFile.getName() + fc.getFileFilter());
-      }
-      if (saveFile.exists()) {
-        String s1 = "Overwrite";
-        String s2 = "Cancel";
-        Object[] options = {s1, s2};
-        if (JOptionPane.showOptionDialog(
-                Cooja.getTopParentContainer(),
-                "A file with the same name already exists.\nDo you want to remove it?",
-                "Overwrite existing file?", JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE, null, options, s1) != JOptionPane.YES_OPTION) {
-          return null;
-        }
-      }
-      if (saveFile.exists() && !saveFile.canWrite()) {
-      	JOptionPane.showMessageDialog(
-      			getTopParentContainer(), "No write access to " + saveFile, "Save failed",
-      			JOptionPane.ERROR_MESSAGE);
-        logger.fatal("No write access to file: " + saveFile.getAbsolutePath());
+    File saveFile = fc.getSelectedFile();
+    if (!fc.accept(saveFile)) {
+      saveFile = new File(saveFile.getParent(), saveFile.getName() + fc.getFileFilter());
+    }
+    if (saveFile.exists()) {
+      String s1 = "Overwrite";
+      String s2 = "Cancel";
+      Object[] options = {s1, s2};
+      if (JOptionPane.showOptionDialog(getTopParentContainer(),
+              "A file with the same name already exists.\nDo you want to remove it?",
+              "Overwrite existing file?", JOptionPane.YES_NO_OPTION,
+              JOptionPane.QUESTION_MESSAGE, null, options, s1) != JOptionPane.YES_OPTION) {
         return null;
       }
-      saveSimulationConfig(saveFile);
-      addToFileHistory(saveFile);
-      return saveFile;
     }
-    return null;
+    if (saveFile.exists() && !saveFile.canWrite()) {
+      JOptionPane.showMessageDialog(getTopParentContainer(),
+            "No write access to " + saveFile, "Save failed", JOptionPane.ERROR_MESSAGE);
+      logger.fatal("No write access to file: " + saveFile.getAbsolutePath());
+      return null;
+    }
+    saveSimulationConfig(saveFile);
+    addToFileHistory(saveFile);
+    return saveFile;
   }
 
   /** Allocate a file chooser for Cooja simulation files. */
