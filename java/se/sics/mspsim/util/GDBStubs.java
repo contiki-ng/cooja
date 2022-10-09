2d9bcfb3dd2055711f2b85e6fcc74cee10ce2256
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-10-09 14:07:18 +0200

    GDBStubs: remove unused method

--- java/se/sics/mspsim/util/GDBStubs.java
+++ java/se/sics/mspsim/util/GDBStubs.java
@@ -198,29 +198,6 @@ public class GDBStubs implements Runnable {
         sendResponse(regs.toString());
     }
 
-    public static String stringToHex(String base)
-    {
-        StringBuilder buffer = new StringBuilder();
-        int intValue;
-        for(int x = 0; x < base.length(); x++)
-        {
-            int cursor = 0;
-            intValue = base.charAt(x);
-            String binaryChar = Integer.toBinaryString(base.charAt(x));
-            for(int i = 0; i < binaryChar.length(); i++) {
-                if(binaryChar.charAt(i) == '1') {
-                    cursor += 1;
-                }
-            }
-            if((cursor % 2) > 0) {
-                intValue += 128;
-            }
-            buffer.append(Integer.toHexString(intValue));
-        }
-        return buffer.toString();
-    }
-
-
     public void sendResponse(String resp) throws IOException {
         output.write('$');
         int cs = 0;
