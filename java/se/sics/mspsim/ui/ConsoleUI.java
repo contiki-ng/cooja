730fb929ec2253d7478a47876dfdb6faa6da4e53
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-10-24 18:18:06 +0200

    ConsoleUI: remove unnecessary toString

--- java/se/sics/mspsim/ui/ConsoleUI.java
+++ java/se/sics/mspsim/ui/ConsoleUI.java
@@ -306,7 +306,7 @@ public class ConsoleUI extends JComponent {
       String line = new String(chars, 0, len);
       if (line.trim().length() > 0)
         history[pos++] = line;
-      addLine(currentOutput.toString() + line);
+      addLine(currentOutput + line);
       if (pos >= history.length)
         pos = 0;
       back = 0;
@@ -407,7 +407,7 @@ public class ConsoleUI extends JComponent {
         fetchLine += lineCount == lines.length ? lines.length : 1;
     }
     /* actual last visible line */
-    String ll = currentOutput.toString() + new String(chars, 0, len);
+    String ll = currentOutput + new String(chars, 0, len);
     while (scrLine >= 0 && fetchLine >= 0 && ll != null) {
       int neededRows = 1 + (ll.length() / lineWidth);
       int lastStartPos = (neededRows - 1) * lineWidth;
