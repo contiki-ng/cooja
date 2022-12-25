396da512051043068e00441921632de157635499
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2022-12-25 12:00:45 +0100

    Scan: use try-with-resources

--- java/se/sics/mspsim/extutil/highlight/Scan.java
+++ java/se/sics/mspsim/extutil/highlight/Scan.java
@@ -6,7 +6,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 
 public class Scan {
@@ -24,9 +23,11 @@ public class Scan {
     File file = new File(filename);
     int len = (int) file.length();
     char[] buffer = new char[len];
-    Reader in = Files.newBufferedReader(file.toPath(), UTF_8);
-    in.read(buffer);
-    in.close();
+    try (var in = Files.newBufferedReader(file.toPath(), UTF_8)) {
+      if (in.read(buffer) < 0) {
+        throw new IOException("in.read failed");
+      }
+    }
 
     Scanner scanner = new Scanner();
     scanner.change(0, 0, len);
