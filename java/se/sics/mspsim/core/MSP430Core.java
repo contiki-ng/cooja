9e0bfa41d1b8c6d34f2b454b6a74afdc1adefe35
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-08-09 14:28:07 +0200

    MSP430Core: remove unused method

--- java/se/sics/mspsim/core/MSP430Core.java
+++ java/se/sics/mspsim/core/MSP430Core.java
@@ -351,18 +351,6 @@ public class MSP430Core extends Chip implements MSP430Constants {
       return chips.toArray(new Chip[0]);
   }
 
-  public <T extends Chip> T[] getChips(Class<T> type) {
-      ArrayList<T> list = new ArrayList<>();
-      for(Chip chip : chips) {
-          if (type.isInstance(chip)) {
-              list.add(type.cast(chip));
-          }
-      }
-      @SuppressWarnings("unchecked")
-      T[] tmp = (T[]) java.lang.reflect.Array.newInstance(type, list.size());
-      return list.toArray(tmp);
-  }
-
   public Collection<Loggable> getLoggables() {
       var ls = new ArrayList<Loggable>(ioUnits);
       ls.addAll(chips);
